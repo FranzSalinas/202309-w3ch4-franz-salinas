652afd809a43353e5100f5be48e87cb504ab9aab
Franz <fernandofranquiz@gmail.com>
2023-10-26 23:21:06 +0200

    Add component pending of list

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { Header } from './components/header';
 
+import { Subtection } from './components/subsection';
+import { Pending } from './components/pending';
+import { series } from './data/repo';
+
 function main() {
   console.log('Loaded main');
 
   const appElement = document.querySelector<HTMLDivElement>('.container');
   if (appElement === null) return;
 
-  const components = [new Header('.container')];
+  const components = [
+    new Header('.container'),
+    new Subtection('.main-header'),
+    new Pending('.main', series),
+  ];
 
   console.log(components);
 }
